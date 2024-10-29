69b54a1fd0bd3960599e530dcc70879d2d590825
Adrian <adrian.pina11@gmail.com>
2024-10-29 19:46:50 +0000

    Created routes and worked on the filter page

--- src/api/models.py
+++ src/api/models.py
@@ -7,7 +7,10 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
 
+    gender = db.Column(db.String(80), nullable=False)
     def __repr__(self):
         return f'<User {self.email}>'
 
@@ -15,5 +18,10 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "longitude": self.longitude,
+            "latitude": self.latitude,
+            "gender": self.gender,
             # do not serialize the password, its a security breach
         }
+    
+    
